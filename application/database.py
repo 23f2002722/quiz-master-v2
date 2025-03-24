170e58216e7675dfbfa13f192c755ab575b1c310
sks436 <shivangsingh436@gmail.com>
2025-03-24 10:55:50 +0530

    Initialized private GitHub repository with README and .gitignore

---
+++ application/database.py
@@ -0,0 +1,3 @@
+from flask_sqlalchemy import SQLAlchmey
+
+db=SQLAlchmey()
--- application/models.py
+++ application/models.py
@@ -1,1 +1,106 @@
-from flask_sqlalchemy import SQLAlchemy
+from .database import db
+from datetime import datetime, date, timezone
+from werkzeug.security import generate_password_hash
+from flask_security import UserMixin, RoleMixin
+
+# Users Table - Stores user data
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(120), nullable=False)
+    full_name = db.Column(db.String(100))
+    qualification = db.Column(db.String(100))
+    dob = db.Column(db.Date)
+    created_at = db.Column(db.DateTime, default=datetime.now(timezone.utc))
+
+    fs_uniquifier=db.Column(db.String, unique=True, nullable=False)
+    active=db.Column(db.Boolean, nullable=False)
+
+    roles = db.relationship('Role', backref='bearer' secondary='users_roles')
+    scores = db.relationship('Score', backref='user', lazy=True, cascade="all, delete-orphan")
+
+class Role(db.Model, RoleMixin):
+    id=db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String, unique=True, nullable=False)
+    description=db.Column(db.String)
+
+#many-to-many
+class UsersRoles(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    user_id=db.Column(db.Integer, db.ForeignKey('user.id'))
+    role_id=db.Column(db.Integer, db.ForeignKey('role.id'))
+
+
+# Quizzes Table - Stores quiz data
+class Quiz(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    type=db.Column(db.String(50))
+    chapter_id = db.Column(db.Integer, db.ForeignKey('chapter.id'), nullable=False)
+    date_of_quiz = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    time_duration = db.Column(db.Integer)  # format: HH:MM
+    remarks = db.Column(db.String(255))
+
+    questions = db.relationship('Question', backref='quiz', lazy=True, cascade="all, delete-orphan")
+    scores = db.relationship('Score', backref='quiz', lazy=True, cascade="all, delete-orphan")
+
+
+# Chapters Table - Stores chapter data
+class Chapter(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    subject_id = db.Column(db.Integer, db.ForeignKey('subject.id'), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(255))
+
+    quizzes = db.relationship('Quiz', backref='chapter', lazy=True, cascade="all, delete-orphan")
+
+    def __repr__(self):
+        return f'<Chapter {self.name}>'
+
+# Subjects Table - Stores subject data
+class Subject(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(255))
+
+    chapters = db.relationship('Chapter', backref='subject', lazy=True, cascade="all, delete-orphan")
+
+
+# Questions Table - Stores quiz questions
+class Question(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    quiz_id = db.Column(db.Integer, db.ForeignKey('quiz.id'), nullable=False)
+    question_statement = db.Column(db.String(255), nullable=False, unique=True)
+    option1 = db.Column(db.String(100), nullable=False)
+    option2 = db.Column(db.String(100), nullable=False)
+    option3 = db.Column(db.String(100), nullable=False)
+    option4 = db.Column(db.String(100), nullable=False)
+    correct_option = db.Column(db.String(100), nullable=False)
+
+
+# Scores Table - Stores user's quiz attempt results
+class Score(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    quiz_id = db.Column(db.Integer, db.ForeignKey('quiz.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    total_score = db.Column(db.Integer, nullable=False)
+
+    __table_args__ = (db.UniqueConstraint('quiz_id', 'user_id', name='unique_user_quiz_attempt'),)
+
+
+
+with app.app_context():
+    db.create_all()
+    admin = User.query.filter_by(role="admin").first()
+    if not admin:
+        password_hash = generate_password_hash("987")
+        admin = User(
+            username="admin@gmail.com",
+            full_name="admin",
+            qualification="Graduation",
+            dob=date(2004,12,16),
+            password_hash=password_hash,
+            role="admin",
+        )
+        db.session.add(admin)
+        db.session.commit()
