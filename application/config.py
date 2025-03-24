f9c563e5189b70f0a308316f39ccd127b2fa54f7
sks436 <shivangsingh436@gmail.com>
2025-03-24 10:55:21 +0530

    Initialized private GitHub repository with README and .gitignore

---
+++ application/config.py
@@ -0,0 +1,13 @@
+class Config():
+    DEBUG=False
+    SQLALCHEMY_TRACK_MODIFICATIONS=True
+
+class LocalDevelopmentConfig(Config):
+    SQLALCHEMY_DATABASE_URI="sqlite:///db.sqlite3"
+    DEBUG=True
+
+    SECRET_KEY="12345"
+    SECURITY+PASSWORD_HASH="bcrypt"
+    SECURITY_PASSWORD_SALT="12345"
+    WTF_CSRF_ENABLED=False
+    SECURITY_TOKEN_AUTHENTICATION_HEADER="Authentication-Token"
